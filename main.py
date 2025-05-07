7cbabcd1c61349dc30c051c01f744be2f3a9fab1
Jon Bonsor-Matthews <jpm66@cam.ac.uk>
2025-05-07 15:16:21 +0100

    Add LED tests

---
+++ main.py
@@ -0,0 +1,10 @@
+from test_led import test_led
+from test_led_pwm import test_led_pwm
+
+print("Welcome to main.py!")
+
+# Uncomment the test to run
+# test_led()
+# test_led_pwm()
+
+print("main.py Done!")
