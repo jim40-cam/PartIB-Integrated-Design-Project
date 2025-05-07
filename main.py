8faeec0eb4ca0143ee2c72c6196461dce7e2b7cd
Jon Bonsor-Matthews <jpm66@cam.ac.uk>
2025-05-07 15:16:33 +0100

    Add Motor test

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from test_led import test_led
 from test_led_pwm import test_led_pwm
+from test_motor import test_motor3
 
 print("Welcome to main.py!")
 
 # Uncomment the test to run
 # test_led()
 # test_led_pwm()
+# test_motor3()
 
 print("main.py Done!")
