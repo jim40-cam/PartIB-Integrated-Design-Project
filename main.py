b4a58eb992eb0ddd33c74f1d98cfd0918afff9d0
Jon Bonsor-Matthews <jpm66@cam.ac.uk>
2025-05-08 11:03:02 +0100

    Add input handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from test_led import test_led
 from test_led_pwm import test_led_pwm
+from test_input import test_input_poll
 from test_motor import test_motor3
 from test_linear_actuator import test_actuator1
 from test_tcs3472 import test_tcs3472
@@ -10,6 +11,7 @@ print("Welcome to main.py!")
 # Uncomment the test to run
 # test_led()
 # test_led_pwm()
+# test_input_poll()
 # test_motor3()
 # test_tcs3472()
 # test_actuator1()
