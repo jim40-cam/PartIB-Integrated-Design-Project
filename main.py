e092403c6d98efc9e8c06f8143ec491cbfe43362
Jon Bonsor-Matthews <jpm66@cam.ac.uk>
2025-05-26 11:56:11 +0100

    Implement IO mode of STU-22L

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from test_tcs3472 import test_tcs3472
 from test_vl53l0x import test_vl53l0x
 from test_mfrc522 import test_mfrc522
 from test_TMF8x01_get_distance import test_TMF8x01_get_distance
+from test_STU_22L_IO_Mode import test_STU_22L_IO_Mode
 
 print("Welcome to main.py!")
 
@@ -20,5 +21,6 @@ print("Welcome to main.py!")
 # test_vl53l0x()
 # test_mfrc522()
 # test_TMF8x01_get_distance()
+# test_STU_22L_IO_Mode()
 
 print("main.py Done!")
