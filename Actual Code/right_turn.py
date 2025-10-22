ac5113fd8fd192ebc07566c922ef01130c7fb0fd
hk576 <hk576@cam.ac.uk>
2025-10-22 10:21:02 +0100

    switching off onedrive sync for this folder temporarily to avoid issues.

--- Actual Code/right_turn.py
+++ Actual Code/right_turn.py
@@ -2,27 +2,7 @@ from machine import Pin, PWM
 from utime import sleep
 import _thread
 import sys
-sys.path.append("c:/Users/super/OneDrive - University of Cambridge/2025-2026 Cam/IEP V2/PartIB-Integrated-Design-Project-1/Actual Code")
-from class_definitions import Motor
-
-class Motor:
-    def __init__(self, dirPin, PWMPin):
-        self.mDir = Pin(dirPin, Pin.OUT)  # set motor direction pin
-        self.pwm = PWM(Pin(PWMPin))  # set motor pwm pin
-        self.pwm.freq(1000)  # set PWM frequency
-        self.pwm.duty_u16(0)  # set duty cycle - 0=off
-        
-    def off(self):
-        self.pwm.duty_u16(0)
-        
-    def Forward(self, speed=100):
-        self.mDir.value(0)                     # forward = 0 reverse = 1 motor
-        self.pwm.duty_u16(int(65535 * speed / 100))  # speed range 0-100 motor
-
-    def Reverse(self, speed=30):
-        self.mDir.value(1)
-        self.pwm.duty_u16(int(65535 * speed / 100))
-
+from .forward_movement import Motor
 
 
 def right():
